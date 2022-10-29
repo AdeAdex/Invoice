193a74db2901bf752699cb3c7df62ea2370cd250
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2022-10-29 08:16:41 +0100

    Add files via upload

---
+++ invoice.js
@@ -0,0 +1,26 @@
+
+/*const press = document.getElementById("button");
+const myBas = document.getElementById("items");
+const yhea = document.getElementById("itemId");
+const yhea2 = document.getElementById("itemDescription");
+const yhea3 = document.getElementById("itemQuantity");
+const yhea4 = document.getElementById("itemPrice");
+const yhea5 = document.getElementById("itemTax");
+const yhea6 = document.getElementById("itemAmount");
+press.addEventListener("click", myFunction)
+function myFunction() {
+  const para = document.createElement("itemsInfo");
+  para.style.display = "flex";
+  para.style.justifyContent = "space-around"; 
+  para.appendChild(yhea);
+  para.appendChild(yhea2);
+  para.appendChild(yhea3);
+  para.appendChild(yhea4);
+  para.appendChild(yhea5);
+  para.appendChild(yhea6);
+  myBas.appendChild(para);
+}
+//
+//yhea.style.display = "flex";
+//
+*/
